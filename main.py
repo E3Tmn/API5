830d5bd5185084999e082e699930b2b9cc8d8e56
E3Tmn <nick1tapinyagin@yandex.ru>
2023-10-08 20:44:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,86 +6,100 @@ import os
 from dotenv import load_dotenv
 
 
+
+def get_array_salaries(salary_from, salary_to):
+    if salary_from and salary_to:
+        average_salary = (salary_from + salary_to)/2
+    elif salary_from and not salary_to:
+        average_salary = salary_from * 1.2
+    else:
+        average_salary = salary_to * 0.8
+    return average_salary
+
+
 def hh_predict_rub_salary(response):
-    salary = []
-    for item in response['items']:
-        salary_from = item["salary"]['from']
-        salary_to = item["salary"]['to']
-        if item["salary"]["currency"] != 'RUR':
-            continue
-        elif salary_from and salary_to:
-            salary.append((salary_from + salary_to)/2)
-        elif salary_from and not salary_to:
-            salary.append(salary_from * 1.2)
-        else:
-            salary.append(salary_to * 0.8)
-    return {'vacancies_processed': len(salary), "average_salary": sum(salary)}
+    salaries = []
+    for vacancy in response['items']:
+        if vacancy["salary"]:
+            salary_from = vacancy["salary"]['from']
+            salary_to = vacancy["salary"]['to']
+            if vacancy["salary"]["currency"] != 'RUR':
+                continue
+            else:
+                salaries.append(get_array_salaries(salary_from, salary_to))
+    return (len(salaries), sum(salaries))
 
 
-def hh_get_vacancies(top_languages):
+def hh_get_vacancies_statistics(top_languages):
     statistic_vacancies = {}
+    number_of_elements = 100
+    region_id = 1
+    number_of_days = 30
     for language in top_languages:
         forecast = Counter()
-        for page in itertools.count(0):
+        for page_number in itertools.count(0):
             url = 'https://api.hh.ru/vacancies/'
             payload = {
                 "text": f'Программист {language}',
-                "per_page": 100,
-                "area": 1,
-                "page": page,
-                "period": 30,
-                'only_with_salary': True
+                "per_page": number_of_elements,
+                "area": region_id,
+                "page": page_number,
+                "period": number_of_days
             }
             response = requests.get(url, params=payload)
             response.raise_for_status()
             vacancies = response.json()
-            forecast += Counter(hh_predict_rub_salary(vacancies))
-            if page + 1 == vacancies['pages']:
+            salary_statistics = hh_predict_rub_salary(vacancies)
+            forecast += Counter({'vacancies_processed': salary_statistics[0], "average_salary": salary_statistics[1]})
+            if page_number + 1 == vacancies['pages']:
                 break
-        forecast["average_salary"] = int(forecast["average_salary"] / forecast["vacancies_processed"])
+        if forecast:
+            forecast["average_salary"] = int(forecast["average_salary"] / forecast["vacancies_processed"])
         medium_salary = {'vacancies_found': vacancies["found"]} | forecast
         statistic_vacancies.setdefault(language, medium_salary)
     return statistic_vacancies
 
 
 def sj_predict_rub_salary(vacancies):
-    salary = []
+    salaries = []
     for item in vacancies['objects']:
         payment_from = item['payment_from']
         payment_to = item['payment_to']
         if item["currency"] != 'rub':
             return None
-        elif payment_from and payment_to:
-            salary.append((payment_from + payment_to)/2)
-        elif payment_from and not payment_to:
-            salary.append(payment_from * 1.2)
         else:
-            salary.append(payment_to * 0.8)
-    return {'vacancies_processed': len(salary), "average_salary": sum(salary)}
+            salaries.append(get_array_salaries(payment_from, payment_to))
+    return (len(salaries), sum(salaries))
 
 
-def sj_get_vacancies(top_languages):
+def sj_get_vacancies_statistics(top_languages, secret_key):
     statistic_vacancies = {}
+    number_of_elements = 100
+    catalog_number = 48
+    salary_type_id = 1
+    number_of_days = 30
+    max_number_of_pages = 5
     for language in top_languages:
         forecast = Counter()
-        for page in range(5):
+        for page_number in range(max_number_of_pages):
             url = 'https://api.superjob.ru/2.0/vacancies/'
             headers = {
-                'X-Api-App-Id': os.environ["SUPERJOB_KEY"]
+                'X-Api-App-Id': secret_key
             }
             payload = {
                 'town': 'Москва',
-                'catalogues': 48,
+                'catalogues': catalog_number,
                 'keyword': f"программист {language}",
-                'no_agreement': 1,
-                'period': 30,
-                "page": page,
-                'count': 100
+                'no_agreement': salary_type_id,
+                'period': number_of_days,
+                "page": page_number,
+                'count': number_of_elements
             }
             response = requests.get(url, params=payload, headers=headers)
             response.raise_for_status()
             vacancies = response.json()
-            forecast += Counter(sj_predict_rub_salary(response.json()))
+            salary_statistics = sj_predict_rub_salary(vacancies)
+            forecast += Counter({'vacancies_processed': salary_statistics[0], "average_salary": salary_statistics[1]})
         if forecast:
             forecast["average_salary"] = int(forecast["average_salary"] / forecast["vacancies_processed"])
         medium_salary = {'vacancies_found': vacancies['total']} | forecast
@@ -94,12 +108,12 @@ def sj_get_vacancies(top_languages):
 
 
 def print_table(title, statistic_vacancies):
-    table_data = [['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']]
+    table = [['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']]
     for counter, vacancy in enumerate(statistic_vacancies):
-            table_data.append([vacancy])
-            for key, value in statistic_vacancies[vacancy].items():
-                table_data[counter + 1].append(value)
-    table = AsciiTable(table_data, title)
+            table.append([vacancy])
+            for requirement, parameter in statistic_vacancies[vacancy].items():
+                table[counter + 1].append(parameter)
+    table = AsciiTable(table, title)
     print(table.table)
     return
 
@@ -108,8 +122,8 @@ def main():
     load_dotenv()
     secret_key = os.environ["SUPERJOB_KEY"]
     top_languages = ['Javascript', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'CSS', 'C#', 'C', 'GO']
-    print_table('HeadHunter Moscow', hh_get_vacancies(top_languages))
-    print_table('SuperJob Moscow', sj_get_vacancies(top_languages))
+    print_table('HeadHunter Moscow', hh_get_vacancies_statistics(top_languages))
+    print_table('SuperJob Moscow', sj_get_vacancies_statistics(top_languages, secret_key))
 
 
 if __name__ == "__main__":
